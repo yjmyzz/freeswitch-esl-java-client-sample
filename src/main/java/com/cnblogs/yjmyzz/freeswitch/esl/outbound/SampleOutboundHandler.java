bc79586faeccfef8c95a8e89acf2013760309efc
yjmyzz@126.com <2city@4tory>
2020-02-09 18:23:56 +0800

    add blacklist demo

--- src/main/java/com/cnblogs/yjmyzz/freeswitch/esl/outbound/SampleOutboundHandler.java
+++ src/main/java/com/cnblogs/yjmyzz/freeswitch/esl/outbound/SampleOutboundHandler.java
@@ -10,6 +10,7 @@ import org.jboss.netty.channel.ChannelHandlerContext;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * @author 菩提树下的杨过
@@ -29,21 +30,70 @@ public class SampleOutboundHandler extends AbstractOutboundClientHandler {
             System.out.println("Caller-Destination-Number: " + event.getEventHeaders().get("Caller-Destination-Number"));
             System.out.println("=======================  = = = = = = = = = = =  =============================");
 
-            // now bridge the call
-            bridgeCall(ctx.getChannel(), event);
+            //客人黑名单检测示例
+            String callerNumber = event.getEventHeaders().get("Caller-Caller-ID-Number");
+            if (callerNumber.equalsIgnoreCase("1002")) {
+                System.out.println(callerNumber + " is in blacklist !");
+                //挂断
+                hangupCall(ctx.getChannel());
 
+            } else {
+                //放音乐
+                playSound(ctx.getChannel(), event);
+
+                //转接到客服
+                bridgeCall(ctx.getChannel(), event);
+            }
 
         } else {
             throw new IllegalStateException("Unexpected event after connect: [" + event.getEventName() + ']');
         }
     }
 
+    private void hangupCall(Channel channel) {
+        SendMsg hangupMsg = new SendMsg();
+        hangupMsg.addCallCommand("execute");
+        hangupMsg.addExecuteAppName("hangup");
+
+        EslMessage response = sendSyncMultiLineCommand(channel, hangupMsg.getMsgLines());
+        if (response.getHeaderValue(EslHeaders.Name.REPLY_TEXT).startsWith("+OK")) {
+            System.out.println("Call hangup successful");
+        } else {
+            System.out.println("Call hangup failed: " + response.getHeaderValue(EslHeaders.Name.REPLY_TEXT));
+        }
+    }
+
+    private void playSound(Channel channel, EslEvent event) {
+        String uuid = event.getEventHeaders().get("Unique-ID");
+        SendMsg playbackMsg = new SendMsg(uuid);
+        playbackMsg.addCallCommand("execute");
+        playbackMsg.addExecuteAppName("playback");
+        playbackMsg.addExecuteAppArg("/Users/jimmy/Downloads/bgm.wav");
+        //同步发送bridge命令接通
+        EslMessage response = sendSyncMultiLineCommand(channel, playbackMsg.getMsgLines());
+        if (response.getHeaderValue(EslHeaders.Name.REPLY_TEXT).startsWith("+OK")) {
+            System.out.println("playback successful");
+        } else {
+            System.out.println("playback failed: " + response.getHeaderValue(EslHeaders.Name.REPLY_TEXT));
+        }
+    }
+
+
     private void bridgeCall(Channel channel, EslEvent event) {
+        //模拟接通前，客人需要等待n秒
+        try {
+            Random rnd = new Random(System.currentTimeMillis());
+            Thread.sleep(1 + rnd.nextInt(20) * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         List<String> extNums = new ArrayList<>(2);
         extNums.add("1000");
         extNums.add("1010");
-        //随机找1个目标
+        //随机找1个目标接通
         String destNumber = extNums.get((int) System.currentTimeMillis() % 2);
+
 
         SendMsg bridgeMsg = new SendMsg();
         bridgeMsg.addCallCommand("execute");
